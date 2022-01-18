24faf39b47c732162db56b89a46a74af6738c7d5
vaishnavi3PG <vaishnavi.shivhare@3pillarglobal.com>
2022-01-18 21:45:12 +0530

    done

---
+++ keeper-app-part-1/src/component/Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+    return (
+        <div className="note">
+            <h1>
+                This is the Title
+            </h1>
+            <p>
+                This is the Content
+            </p>
+        </div>
+    )
+}
+
+export default Note;
