a0b5239884ac084b6c1d67f61ce90993b36c0a28
vaishnavi3PG <vaishnavi.shivhare@3pillarglobal.com>
2022-01-19 12:27:33 +0530

    part-2 commited

---
+++ keeper-app-part-2/src/component/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Heading(){
+    return(
+        <header>
+            <h1>
+                Keeper App
+            </h1>
+        </header>
+    )
+}
+
+export default Heading;
